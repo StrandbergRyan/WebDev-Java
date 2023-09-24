0c8925c568bb3745464339b3c60fe2f8d0a8bce8
StrandbergRyan <strandbergryanp@gmail.com>
2023-09-24 14:52:52 -0700

    3 Pokemon Array
    I made an Array of 3 Pokemon, adding their name, height, and type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,7 @@
-let favoriteFood = 'Burgers';
-document.write(favoriteFood);
+let pokemonList = [
+    {Name: 'Bulbasaur', height: 7, type: ['grass', 'poison']}, 
+    {Name: 'Charmander', height: 6, type: ['fire']},
+    {Name: 'Squirtle', height: 5, type: ['water']}
+]
+
+document.write(pokemonList[0, 1, 2])
