5517e71b57e27f0029e66243d70514119def57b4
StrandbergRyan <strandbergryanp@gmail.com>
2023-09-27 16:20:52 -0700

    1.3 Progress Update
    Created recommended earlier display method from 1.2, and made a for-if-else statement for displaying as well, although it is flawed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 let pokemonList = [
-    {Name: 'Bulbasaur', height: 7, type: ['grass', 'poison']}, 
-    {Name: 'Charmander', height: 6, type: ['fire']},
-    {Name: 'Squirtle', height: 5, type: ['water']}
+    {Name: 'Bulbasaur', height: 0.7, type: ['grass', ' poison']}, 
+    {Name: 'Charmander', height: 0.6, type: ['fire']},
+    {Name: 'Blastoise', height: 1.6, type: ['water']}
 ]
 
-document.write(pokemonList[0, 1, 2])
+pokemonList.forEach(function(pokemon) {
+    document.write(pokemon.Name + ' (height: ' + pokemon.height + ')'+ ' (type: ' + pokemon.type + ')');
+    document.write('<br>');
+})
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.0){ //if pokemon's height is over 100cm also print 'Wow that's big
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m) - Wow, that\'s big!<br>');
+    }else {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)<br>');
+    }
+}
