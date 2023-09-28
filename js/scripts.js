1b0e158bb50890b33be7df08a2d034774dc765c3
StrandbergRyan <strandbergryanp@gmail.com>
2023-09-28 15:45:26 -0700

    Fixed Undefined Name Issue

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,10 @@ let pokemonList = [
     {Name: 'Blastoise', height: 1.6, type: ['water']}
 ]
 
-pokemonList.forEach(function(pokemon) {
-    document.write(pokemon.Name + ' (height: ' + pokemon.height + ')'+ ' (type: ' + pokemon.type + ')');
-    document.write('<br>');
-})
-
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1.0){ //if pokemon's height is over 100cm also print 'Wow that's big
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m) - Wow, that\'s big!<br>');
+    document.write(pokemonList[i].Name + ' (height: ' + pokemonList[i].height + ' m) - Wow, that\'s big!<br>');
     }else {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)<br>');
+    document.write(pokemonList[i].Name + ' (height: ' + pokemonList[i].height + ' m)<br>');
     }
 }
