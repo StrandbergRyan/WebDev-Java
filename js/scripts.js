d0292ad300781d6d2c197ad3cf9060454a02c157
StrandbergRyan <strandbergryanp@gmail.com>
2023-11-09 16:33:11 -0800

    1.6 Progress

--- js/scripts.js
+++ js/scripts.js
@@ -23,14 +23,23 @@ function addListItem(pokemon){
     button.classList.add('button-class');
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
-    button.addEventListener('click', function() {
+    button.addEventListener('click', () => {
         showDetails(pokemon.name);
     });
+    listItem.classList.add("list-group-item");
+    button.classList.remove("button-class");
+    button.classList.add("btn btn-outline-info");
+  }
 }
 
 function showDetails(pokemon){
     console.log(pokemon) ;
 }
+
+let button = document.querySelector('button');
+button.addEventListener('click', function (event) {
+  console.log(event);
+});
 
 return {
     getAll: getAll,
@@ -41,4 +50,74 @@ return {
 })();
 pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
+}); 
+
+ Add?  function loadList() {
+    return fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url,
+          imageUrl: item.myImage,
+        };
+        add(pokemon);
+      });
+    })
+    .catch(function (e) {
+      console.error(e);
+    });
+}
+
+function loadDetails(item) {
+  let url = item.detailsUrl;
+  return fetch(url)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    })
+    .catch(function (e) {
+      console.error(e);
+    });
+}
+
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+
+function showDetails(pokemon) {
+  loadDetails(pokemon).then(function () {
+    showModal(pokemon);
+  });
+}
+
+return {
+  getAll: function () {
+    return pokemonList;
+  },
+  add,
+  addListItem: addListItem,
+  loadList: loadList,
+  loadDetails: loadDetails,
+};
+})();
+
+let form = document.querySelector('form');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  // Do something manually, for example, add custom validations
+  form.submit();
+});
+
+pokemonRepository.loadList().then(function () {
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
 }); 
