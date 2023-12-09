b6b3bf12e55f3c7fc2bd68119c093b6fec31b9e0
StrandbergRyan <strandbergryanp@gmail.com>
2023-12-08 16:27:14 -0800

    1.7 changes made
    Polyfills and other changes added to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,12 @@
 let pokemonRepository = (function () {
   // Create a pokemonList array variable, housed in an IIFE to keep it unaccessable
   let pokemonList = [
-    {Name: 'Bulbasaur', height: 0.7, type: ['grass', ' poison']}, 
-    {Name: 'Charmander', height: 0.6, type: ['fire']},
-    {Name: 'Blastoise', height: 1.6, type: ['water']}
+    // {Name: 'Bulbasaur', height: 0.7, type: ['grass', ' poison']}, 
+    // {Name: 'Charmander', height: 0.6, type: ['fire']},
+    // {Name: 'Blastoise', height: 1.6, type: ['water']}
     ];
+
+    let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
   // Local function that adds a pokemon object to the array variable pokemonList
   function add(pokemon) {
@@ -50,9 +52,47 @@ let pokemonRepository = (function () {
     listItem.appendChild(button);
     pokePageList.appendChild(listItem)
   }
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+            // imageUrl: "",
+          };
+
+          // console.log(pokemon);
+          add(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
 
   function showDetails(pokemon) {
     console.log(pokemon);
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
   }
 
   // Return statement, allowing you to call add and getAll local functions
@@ -61,6 +101,7 @@ let pokemonRepository = (function () {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
     showDetails: showDetails
   };
 })();
